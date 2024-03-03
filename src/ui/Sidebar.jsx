f83e0f044f1d2b7f9920dbef8eaccc457b2e8b01
Spook <spook.devv@gmail.com>
2024-03-02 19:51:43 -0500

    Building the Sidebar and Main Navigation

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 // grid-row: 'span it from the first row all the way to the last row' ( '1 / -1' )
 const StyledSidebar = styled.aside`
@@ -7,10 +9,18 @@ const StyledSidebar = styled.aside`
   border-right: 1px solid var(--color-grey-100);
 
   grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
-  return <StyledSidebar>SIDEBAR</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
